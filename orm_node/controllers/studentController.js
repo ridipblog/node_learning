54e459614e6d9e290565a39ee8681a31b42f384f
unknown <memorytemp5@gmail.com>
2023-08-23 19:32:34 +0530

    23-08-23

--- orm_node/controllers/studentController.js
+++ orm_node/controllers/studentController.js
@@ -1,159 +1,189 @@
-const { response } = require('express');
-var db=require('../models/conn.js');
-const Students=db.students;
-const {Sequelize,Op}=require('sequelize');
-var addStudent=async (req,res)=>{
-    // let data=await Students.build({name:'Coder 2',email:'coder2@gamil.com'});\
-    // await data.save();
-    let data=await Students.create({name:'coder 7',email:'coder7@gmail.com'});
+const { response } = require("express");
+var db = require("../models/conn.js");
+const Students = db.students;
+const { Sequelize, Op } = require("sequelize");
+var addStudent = async (req, res) => {
+  // let data=await Students.build({name:'Coder 2',email:'coder2@gamil.com'});\
+  // await data.save();
+  let data = await Students.create({
+    name: "coder 7",
+    email: "coder7@gmail.com",
+  });
 
-    // if you update cvalue on runtime 
-    // data.name='temp';
-    // data.save();
+  // if you update cvalue on runtime
+  // data.name='temp';
+  // data.save();
 
-    // if you delete value on run time
-    data.destroy();
-    console.log(data.dataValues);
+  // if you delete value on run time
+  data.destroy();
+  console.log(data.dataValues);
 
-    let response={
-        data:'Ok'
-    }
-    res.status(200).json(response)
-}
-var crudOpearation=async (req,res)=>{
-    // Data Insert
+  let response = {
+    data: "Ok",
+  };
+  res.status(200).json(response);
+};
+var crudOpearation = async (req, res) => {
+  // Data Insert
 
-    // let data=await Students.create({name:'coder 8',email:'coder8@gmail.com'});
-    
+  // let data=await Students.create({name:'coder 8',email:'coder8@gmail.com'});
 
-    // Data Upadted 
+  // Data Upadted
 
-    // let data=await Students.update({name:'coder 2',gender:'male'},{
-    //     where:{
-    //         id:1
-    //     }
-    // });
+  // let data=await Students.update({name:'coder 2',gender:'male'},{
+  //     where:{
+  //         id:1
+  //     }
+  // });
 
-    // Data Deleted 
+  // Data Deleted
 
-    // let data=await Students.destroy({
-    //     where:{
-    //         id:2
-    //     }
-    // });
+  // let data=await Students.destroy({
+  //     where:{
+  //         id:2
+  //     }
+  // });
 
-    // Data Truncate
+  // Data Truncate
 
-    // let data=await Students.destroy({
-    //     truncate:true
-    // });
+  // let data=await Students.destroy({
+  //     truncate:true
+  // });
 
-    // Find Data
+  // Find Data
 
-    // let data=await Students.findAll({});
-    let data=await Students.findOne({});
-    let response={
-        data:data
-    }
-    res.status(200).json(response);
-}
-var queryData=async(req,res)=>{
-    // Insert Specific Filed
-    
-    // let data=await Students.create({name:'query 1',email:'query1@gmail.com',gender:'male'},{
-    //     fields:['email']
-    // })
+  // let data=await Students.findAll({});
+  let data = await Students.findOne({});
+  let response = {
+    data: data,
+  };
+  res.status(200).json(response);
+};
+var queryData = async (req, res) => {
+  // Insert Specific Filed
 
-    // Select Data
+  // let data=await Students.create({name:'query 1',email:'query1@gmail.com',gender:'male'},{
+  //     fields:['email']
+  // })
 
-    // let data=await Students.findAll({
-    //     attributes:[
-    //         'name',
-    //         ['email','emailID']
-    //     ]
-    // });
+  // Select Data
 
-    // let data=await Students.findAll({
-    //     attributes:[
-    //         'name',
-    //         ['email','emailID'],
-    //         [Sequelize.fn('Count',Sequelize.col('email')),'emailCount']
-    //     ]
+  // let data=await Students.findAll({
+  //     attributes:[
+  //         'name',
+  //         ['email','emailID']
+  //     ]
+  // });
 
-    // })
+  // let data=await Students.findAll({
+  //     attributes:[
+  //         'name',
+  //         ['email','emailID'],
+  //         [Sequelize.fn('Count',Sequelize.col('email')),'emailCount']
+  //     ]
 
-    // let data=await Students.findAll({
-    //     attributes:[
-    //         'name',
-    //         ['email','emailID'],
-    //         [Sequelize.fn('CONCAT',Sequelize.col('email'),' ID'),'emailID']
-    //     ]
-    // });
+  // })
 
-    // let data=await Students.findOne({
-    //     where:{
-    //         id:1
-    //     }
-    // });
+  // let data=await Students.findAll({
+  //     attributes:[
+  //         'name',
+  //         ['email','emailID'],
+  //         [Sequelize.fn('CONCAT',Sequelize.col('email'),' ID'),'emailID']
+  //     ]
+  // });
 
-    // incldue and exclude
+  // let data=await Students.findOne({
+  //     where:{
+  //         id:1
+  //     }
+  // });
 
-    // let data=await Students.findAll({
-    //     attributes:{
-    //         exclude:[
-    //             'createdAt',
-    //             'updatedAt'
-    //         ],
-    //         include:[
-    //             [
-    //                 Sequelize.fn('CONCAT',Sequelize.col('name'),' Honda'),'FullName'
-    //             ]
-    //         ]
-    //     }
-    // })
-    // Opearator 
+  // incldue and exclude
 
-    // let data=await Students.findAll({
-    //     where:{
-    //         id:{
-    //             [Op.eq]:1
-    //         },
-    //         name:{
-    //             [Op.eq]:'coder 2'
-    //         },
-    //         email:{
-    //             [Op.like]:'%@gamil.com'
-    //         }
-    //     }
-    // })
+  // let data=await Students.findAll({
+  //     attributes:{
+  //         exclude:[
+  //             'createdAt',
+  //             'updatedAt'
+  //         ],
+  //         include:[
+  //             [
+  //                 Sequelize.fn('CONCAT',Sequelize.col('name'),' Honda'),'FullName'
+  //             ]
+  //         ]
+  //     }
+  // })
+  // Opearator
 
-    // let data=await Students.findAll({
-    //     where:{
-    //         [Op.or]:[
-    //             {id:3},
-    //             {name:'code'}
-    //         ]
-    //     }
-    // })
+  // let data=await Students.findAll({
+  //     where:{
+  //         id:{
+  //             [Op.eq]:1
+  //         },
+  //         name:{
+  //             [Op.eq]:'coder 2'
+  //         },
+  //         email:{
+  //             [Op.like]:'%@gamil.com'
+  //         }
+  //     }
+  // })
 
-    let data=await Students.findAll({
-        where:{
-            email:{
-                [Op.like]:'%@gmail.com'
-            }
-        },
-        order:[
-            ['name','DESC']
-        ],
-        limit:2
-    })
-    let response={
-        data:data
-    }
-    res.status(200).json(response)
-}
-module.exports={
-    addStudent,
-    crudOpearation,
-    queryData
-}
+  // let data=await Students.findAll({
+  //     where:{
+  //         [Op.or]:[
+  //             {id:3},
+  //             {name:'code'}
+  //         ]
+  //     }
+  // })
+
+  let data = await Students.findAll({
+    where: {
+      email: {
+        [Op.like]: "%@gmail.com",
+      },
+    },
+    order: [["name", "DESC"]],
+    limit: 2,
+  });
+  let response = {
+    data: data,
+  };
+  res.status(200).json(response);
+};
+var finderQuery = async (req, res) => {
+  //   let data = await Students.findAll({});
+  //   let data = await Students.findOne({});
+  //   let data = await Students.findByPk(4);
+  //   let data = await Students.findAndCountAll({
+  //     where: {
+  //       email: "query1@gmail.com",
+  //     },
+  //   });
+  let [data, created] = await Students.findOrCreate({
+    where: { name: "dummy" },
+    defaults: {
+      email: "dummy@gamil.com",
+      gender: "male",
+    },
+  });
+  let response = {
+    data: data,
+    add: created,
+  };
+  res.status(200).json(response);
+};
+var setGetQuery = (req, res) => {
+  let response = {
+    data: "Setter Getter",
+  };
+  res.status(200).json(response);
+};
+module.exports = {
+  addStudent,
+  crudOpearation,
+  queryData,
+  finderQuery,
+  setGetQuery,
+};
--- orm_node/index.js
+++ orm_node/index.js
@@ -1,11 +1,13 @@
-const express=require('express');
-const app=express();
-require('./models/conn');
-var studentContr=require('./controllers/studentController');
-app.get('/',(req,res)=>{
-    res.send('home');
+const express = require("express");
+const app = express();
+require("./models/conn");
+var studentContr = require("./controllers/studentController");
+app.get("/", (req, res) => {
+  res.send("home");
 });
-app.get('/add',studentContr.addStudent);
-app.get('/crud',studentContr.crudOpearation);
-app.get('/query',studentContr.queryData);
+app.get("/add", studentContr.addStudent);
+app.get("/crud", studentContr.crudOpearation);
+app.get("/query", studentContr.queryData);
+app.get("/finder", studentContr.finderQuery);
+app.get("/setter-getter", studentContr.setGetQuery);
 app.listen(3000);
